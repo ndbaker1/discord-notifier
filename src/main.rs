4ef992619b4fe9d3774246bb6d976b42f67dee65
Nick Baker <ndbaker1@outlook.com>
2023-03-18 07:49:04 +0000

    add dm flag to allow server channel messages

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use structopt::StructOpt;
 
 #[derive(structopt::StructOpt)]
 struct Args {
-    /// Discord User ID
+    /// Discord User ID or Channel to send a message to
     #[structopt(short = "u", long, env = "DISCORD_USER_ID")]
     user: String,
     /// Discord Bot Token
@@ -17,6 +17,9 @@ struct Args {
     /// Flag to control whether stdin should be read to contruct the message
     #[structopt(short = "r", long)]
     read_stdin: bool,
+    /// Whether or not the message is a Direct Message or not
+    #[structopt(short = "d", long)]
+    dm: bool,
 }
 
 fn main() -> Result<()> {
@@ -25,6 +28,7 @@ fn main() -> Result<()> {
         token,
         info,
         read_stdin,
+        dm,
     } = Args::from_args();
 
     // read the entire stdin buffer to use as the output message
@@ -42,10 +46,14 @@ fn main() -> Result<()> {
         .map(|info| format!("> {info}\n```\n{input}\n```"))
         .unwrap_or(input);
 
-    send_message(&token, &user, &content)
+    if dm {
+        send_message_dm(&token, &user, &content)
+    } else {
+        send_message_channel(&token, &user, &content)
+    }
 }
 
-fn send_message(token: &str, user: &str, content: &str) -> Result<()> {
+fn send_message_dm(token: &str, user: &str, content: &str) -> Result<()> {
     // setup a client to use for HTTP requests
     let client = reqwest::blocking::Client::new();
 
@@ -64,6 +72,23 @@ fn send_message(token: &str, user: &str, content: &str) -> Result<()> {
         .as_str()
         .unwrap()
         .to_string();
+
+    client
+        .post(format!(
+            "https://discord.com/api/v8/channels/{channel_id}/messages",
+        ))
+        .json(&HashMap::<_, _>::from_iter(
+            [("content", content)].into_iter(),
+        ))
+        .header("authorization", format!("Bot {token}"))
+        .send()?;
+
+    Ok(())
+}
+
+fn send_message_channel(token: &str, channel_id: &str, content: &str) -> Result<()> {
+    // setup a client to use for HTTP requests
+    let client = reqwest::blocking::Client::new();
 
     client
         .post(format!(
